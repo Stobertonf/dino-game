af2e2c7f10d9bc808aad836d355f84010b877afe
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-03-27 14:23:24 -0300

    feat(Jump-Dino): Add Jump function

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 const dino = document.querySelector('.dino');
+
+function jump() {
+    isJumping = true;
+  
+    let upInterval = setInterval(() => {
+      if (position >= 150) {
+        // Descendo
+        clearInterval(upInterval);
+  
+        let downInterval = setInterval(() => {
+          if (position <= 0) {
+            clearInterval(downInterval);
+            isJumping = false;
+          } else {
+            position -= 20;
+            dino.style.bottom = position + 'px';
+          }
+        }, 20);
+      } else {
+        // Subindo
+        position += 20;
+        dino.style.bottom = position + 'px';
+      }
+    }, 20);
+  }   
